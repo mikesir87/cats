cb004cdd4334365bfd7f5e4e762bde98aba793b8
Michael Irwin <mikesir87@gmail.com>
2019-03-05 21:43:41 -0500

    Update images to ones that actually work

--- app.py
+++ app.py
@@ -8,18 +8,16 @@ container_hostname = socket.gethostname()
 
 # list of cat images
 images = [
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr01/15/9/anigif_enhanced-buzz-31540-1381844535-8.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26390-1381844163-18.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-1376-1381846217-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3391-1381844336-26.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-29111-1381845968-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3409-1381844582-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr02/15/9/anigif_enhanced-buzz-19667-1381844937-10.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26358-1381845043-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-18774-1381844645-6.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-25158-1381844793-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
+    "https://media.giphy.com/media/cfuL5gqFDreXxkWQ4o/giphy.gif",
+    "https://media.giphy.com/media/W8krmZSDxPIfm/giphy.gif",
+    "https://media.giphy.com/media/UxGNvgpU1hoAw/giphy.gif",
+    "https://media.giphy.com/media/UxGNvgpU1hoAw/giphy.gif",
+    "https://media.giphy.com/media/WLNASdXoq0ZtC/giphy.gif",
+    "https://media.giphy.com/media/HYpZKsyLOn1ks/giphy.gif",
+    "https://media.giphy.com/media/l8px6snusvpII/giphy.gif",
+    "https://media.giphy.com/media/l0Iy9Kry5yhBSwQSs/giphy.gif",
+    "https://media.giphy.com/media/7SfAXqgRgh0li/giphy.gif",
+    "https://media.giphy.com/media/W80Y9y1XwiL84/giphy.gif"
 ]
 
 @app.route('/')
